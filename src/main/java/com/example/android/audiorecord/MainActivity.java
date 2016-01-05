6c5dfef5da29dd4148d288ec295119c8fab1d172
ksunho <ksunho0660@kaist.ac.kr>
2016-01-05 17:23:33 +0900

    second version _ pcm data

--- src/main/java/com/example/android/audiorecord/MainActivity.java
+++ src/main/java/com/example/android/audiorecord/MainActivity.java
@@ -1,163 +1,155 @@
 package com.example.android.audiorecord;
 
+import android.media.AudioFormat;
 import android.media.AudioRecord;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.media.audiofx.NoiseSuppressor;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.TextView;
 
-import java.awt.font.TextAttribute;
-import java.io.File;
-import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = "AudioRecord";
-    
-	public static final String filedir = Environment.getExternalStorageDirectory().getAbsolutePath();
-	private static String filename = null;
-    TextView textView;
-    Button play,record;
-    public int D = 0;
-    final Handler handler = new Handler();
+    private static final int RECORDER_SAMPLERATE = 44100;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
 
-   // int buffersize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
-   // NoiseSuppressor.create()
-    public int test_number=0;
-    private MediaRecorder recorder = null;
-    private MediaPlayer player = null;
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-		Log.d("TIMER", filedir);
-		
 
-        play = (Button) findViewById(R.id.play);
-        record = (Button) findViewById(R.id.record);
+        setButtonHandlers();
+        enableButtons(false);
+        System.out.println("testsss------------------");
+        int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+    }
 
-        //timer test
+    private void setButtonHandlers() {
+        ((Button) findViewById(R.id.btnStart)).setOnClickListener(btnClick);
+        ((Button) findViewById(R.id.btnStop)).setOnClickListener(btnClick);
+    }
 
-        textView = (TextView)findViewById(R.id.test_number_view);
+    private void enableButton(int id, boolean isEnable) {
+        ((Button) findViewById(id)).setEnabled(isEnable);
+    }
 
-        record.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                if (recorder == null) {
-					Log.d("TIMER", "____________첫 시작________________");
-                    recorder = new MediaRecorder();
-                    recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-					filename = filedir +  "/test_0.3gp";
-					recorder.setOutputFile(filename);
-                    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-					
-					Log.d("TIMER", "____________시작 근처________________");
-                    try {
-                        recorder.prepare();
-                    } catch (IOException e) {
-                        Log.e(LOG_TAG, "prepare() failed");
-                    }
-					Log.d("TIMER", "____________1111________________");
-                    recorder.start();
-					Log.d("TIMER", "____________2222________________");
-                    record.setText("녹음 중지");
-					
-					final Timer myTimer = new Timer();
-                    final TimerTask myTask = new TimerTask() {
-                        @Override
-                        public void run() {
-                            handler.post(new Runnable() {
-                                public void run() {
-                                    if(D ==1){
-                                        Log.d("TIMER", "____________D 가 1이 되었나?________________");
-                                        myTimer.cancel();
+    private void enableButtons(boolean isRecording) {
+        enableButton(R.id.btnStart, !isRecording);
+        enableButton(R.id.btnStop, isRecording);
+    }
 
-                                    }
-									else{
-                                    Log.d("TIMER", "____________타이머 안에 들어옴!!! 멈출 파일 이름 확인________________");
-                                    Log.d("TIMER", filename);
-                                    recorder.stop();
-									recorder.release();
-									recorder = null;
-									Log.d("TIMER", "____________타이머 안에서 잘 release 되었나?________________");
-									recorder = new MediaRecorder();
-									recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-									recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-									test_number = test_number + 1;\
-                                    textView.setText("" + test_number);
-									filename = filedir +  "/test_" + test_number + ".3gp";
-									recorder.setOutputFile(filename); 
-									recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-									Log.d("TIMER", "____________그 다음 저장할 파일 이름 확인________________");
-									Log.d("TIMER", filename);
-									try {
-										recorder.prepare();
-									} catch (IOException e) {
-										Log.e(LOG_TAG, "prepare() failed");
-									}
-									Log.d("TIMER", "____________그 다음 파일 재 시작!________________");
-									recorder.start();
-									}
-								}
-								});
+    int BufferElements2Rec = 2048; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
 
-                        }
-                    };
-					
-					
-                    myTimer.schedule(myTask, 5000, 5000);
-
-                } else {
-					Log.d("TIMER", "____________녹음 중지 누름!________________");
-                    recorder.stop();
-                    recorder.release();
-
-
-
-					Log.d("TIMER", "____________녹음 최종 마지막 release 누름!________________");
-                    recorder = null;
-                    D = 1;
-                    
-                    record.setText("녹음 시작");
-
-                }
+    private void startRecording() {
+        System.out.println("test??------------------");
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+        System.out.println("test!!------------------");
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                writeAudioDataToFile();
             }
-        });
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    //convert short to byte
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+
+    }
+
+    private void writeAudioDataToFile() {
+        // Write the output audio in byte
+System.out.println("test3------------------");
+        String filePath = "/sdcard/voice8K16bitmono.pcm";
+        short sData[] = new short[BufferElements2Rec];
+        System.out.println("qqq------------------");
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(filePath);
+			System.out.println("test2------------------");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
 
+            recorder.read(sData, 0, BufferElements2Rec);
+            System.out.println("Short wirting to file" + sData.toString());
+            try {
+                // // writes the data to file from buffer
+                // // stores the voice buffer
+                byte bData[] = short2byte(sData);
+                os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    @Override
-        public void onPause(){
-        super.onPause();
-        if(recorder != null){
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+            recorder.stop();
             recorder.release();
             recorder = null;
-
-        }
-
-        if(player != null){
-            player.release();
-            player = null;
+            recordingThread = null;
         }
     }
 
+    private View.OnClickListener btnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnStart: {
+                    enableButtons(true);
+                    startRecording();
+                    break;
+                }
+                case R.id.btnStop: {
+                    enableButtons(false);
+                    stopRecording();
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
+}
 
 
 
